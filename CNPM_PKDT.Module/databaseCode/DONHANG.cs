026f470ed42f7d8d0eaee095ce223da1fb95ca4a
pham phuoc <thanhphuocj3@gmail.com>
2024-11-23 14:04:03 +0700

    Tien uodate db

---
+++ CNPM_PKDT.Module/databaseCode/DONHANG.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using DevExpress.Xpo;
+using DevExpress.Xpo.Metadata;
+using DevExpress.Data.Filtering;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Reflection;
+namespace CNPM_PKDT.Module.database
+{
+
+    public partial class DONHANG
+    {
+        public DONHANG(Session session) : base(session) { }
+        public override void AfterConstruction() { base.AfterConstruction(); }
+    }
+
+}
